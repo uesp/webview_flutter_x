d4d4d9f4e956e45109088056ec72f64f6f847164
TJ <30050459+Jyndigo@users.noreply.github.com>
2026-08-13 16:24:35 +0100

    chore: update flutter

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.13.9/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/james/Developer/wiki_app/dependencies/webview_flutter_plus/example"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/james/Developer/wiki_app/dependencies/webview_flutter_plus/example/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
+export "FLUTTER_APPLICATION_PATH=/Users/james/Developer/wiki_app/dependencies/webview_flutter_x/example"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/james/Developer/wiki_app/dependencies/webview_flutter_x/example/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
